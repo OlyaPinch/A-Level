eb7480d95190ca95ac5ebc1057781fc25bf327d4
Olha Pinchuk <olgapinch@gmail.com>
2023-03-04 01:06:29 +0200

    Module3 HomeTask_3_1

---
+++ Modules/Module3/MyList.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Module3
+{
+    public class MyList<T> 
+    {
+        private T[] myArray = Array.Empty<T>();
+
+        public T this[int index]
+        {
+            get => myArray[index];
+            set => myArray[index] = value;
+        }
+
+        private int position = -1;
+        private int _count;
+
+        public int Count => _count;
+
+        List<int> h = new List<int>();
+
+
+        public T[] Add(T item)
+        {
+            T[] tempArray = new T[myArray.Length + 1];
+            myArray.CopyTo(tempArray, 0);
+            myArray = tempArray;
+            myArray[_count] = item;
+            _count = myArray.Length;
+
+            return myArray;
+        }
+
+        public void Clear()
+        {
+            _count = 0;
+        }
+
+        public T[] AddRange(T[] array)
+        {
+            T[] tempArray = new T[myArray.Length + array.Length];
+
+            myArray.CopyTo(tempArray, 0);
+            array.CopyTo(tempArray, _count);
+            myArray = tempArray;
+            _count = myArray.Length;
+
+            return myArray;
+        }
+
+        public T[] RemoveAt(int index)
+        {
+            T[] tempArray = new T[myArray.Length - 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                if (i < index)
+                {
+                    tempArray[i] = myArray[i];
+                }
+                else
+                {
+                    tempArray[i] = myArray[i + 1];
+                }
+            }
+
+            myArray = tempArray;
+            _count = myArray.Length;
+
+            return myArray;
+        }
+
+        public bool Contain(T item)
+        {
+            bool isExist = false;
+
+
+            for (int i = 0; i < myArray.Length - 1; i++)
+                if (myArray[i].Equals(item))
+                {
+                    isExist = true;
+                    break;
+                }
+
+            return isExist;
+        }
+
+        public int IndexOf(T item)
+        {
+            int index = -1;
+
+            for (int i = 0; i < myArray.Length - 1; i++)
+                if (myArray[i].Equals(item))
+                {
+                    index = i;
+                }
+
+            return index;
+        }
+
+        public bool Remove(T item)
+        {
+            {
+                if (IndexOf(item) != -1)
+                {
+                    this.RemoveAt(IndexOf(item));
+                    return true;
+                }
+                else return false;
+            }
+        }
+
+        public T[] Sort()
+        {
+            Array.Sort(myArray);
+            return myArray;
+        }
+
+
+        public IEnumerator GetEnumerator()
+        {
+            return myArray.GetEnumerator();
+        }
+
+        public void Reset()
+        {
+            position = -1;
+        }
+
+        public void Dispose()
+        {
+            Reset();
+        }
+    }
+}
---
+++ Modules/Module3/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Immutable;
+using System.Reflection;
+using System.Runtime.InteropServices.ComTypes;
+using System.Threading.Channels;
+
+namespace Module3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> myArray = new MyList<int>();
+            myArray.Add(20);
+            myArray.Add(23);
+            myArray.Add(10);
+            myArray.Add(500);
+            foreach (var i in myArray)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            Console.WriteLine(new string('-', 10));
+
+            int[] forAdd = new[] { 1000, 2000, 3000, 4000 };
+            myArray.AddRange(forAdd);
+            foreach (var i in myArray)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            Console.WriteLine(new string('-', 10));
+            myArray.RemoveAt(3);
+            myArray.Remove(20);
+            foreach (var i in myArray)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            Console.WriteLine(new string('-', 10));
+            MyList<Person> personList = new MyList<Person>();
+
+            Person p1 = new Person(3, "CCCC");
+            Person p2 = new Person(2, "BBBB");
+            Person p3 = new Person(1, "AAAA");
+            personList.Add(p1);
+            personList.Add(p2);
+            Person[] clArr = new Person[] { p3, p2 };
+            personList.AddRange(clArr);
+            Console.WriteLine(new string('-', 10));
+            foreach (var i in myArray)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            personList.Sort();
+            foreach (Person ex in personList)
+            {
+                Console.WriteLine($"list for example {ex.Id}  {ex.Name}");
+            }
+
+            myArray.Sort();
+            foreach (var i in myArray)
+            {
+                Console.WriteLine($"{i}");
+            }
+         
+
+        }
+    }
+}
