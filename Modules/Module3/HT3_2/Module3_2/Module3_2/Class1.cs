e075978e683095b8dbee3153c9f63bc1ba9e7831
Olha Pinchuk <olgapinch@gmail.com>
2023-03-08 17:12:30 +0200

    HomeTask3_2Delegates

---
+++ Modules/Module3/HT3_2/Module3_2/Module3_2/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module3_2
+{
+    public delegate void ShowHandler(bool value);
+
+    public class Class1
+    {
+        public ShowHandler ShowHandler = Program.Show;
+
+        public double Pow(int x, int y)
+        {
+            return (double)(Math.Pow(x, y));
+        }
+    }
+}
---
+++ Modules/Module3/HT3_2/Module3_2/Module3_2/Class2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module3_2
+{
+    internal class Class2
+    {
+        public Func<double, bool> ResultDelegate;
+
+
+        private double result;
+
+        public Class2()
+        {
+            ResultDelegate = Result;
+        }
+
+        public Func<double, bool> Calc(Func<int, int, double> PowHadler, int x, int y)
+        {
+            result = PowHadler(x, y);
+            return ResultDelegate;
+        }
+
+        public bool Result(double divisor)
+        {
+            if (divisor == 0)
+            {
+                return false;
+            }
+
+            return result % divisor == 0;
+        }
+    }
+}
