774ca8078f85456b3a997e69f56631c6ae5f324e
Olha Pinchuk <olgapinch@gmail.com>
2023-06-02 23:50:05 +0300

    Module 6_1 Add project Product.Host  with  methods

---
+++ Modules/Module6/Module6_1/eShop-Sample2/eShop-Sample2/DataAccess/Repository.cs
@@ -0,0 +1,53 @@
+﻿using DataObects;
+
+namespace DataAccess
+ 
+{
+    public interface IRepository
+    {
+        List<Product> GetAllProducts();
+        Product GetById(int id);
+        bool Add(Product product);
+    }
+
+    public class Repository : IRepository
+    {
+        List<Product> Products = new List<Product>
+        {
+            new Product() { Id = 1, Name = "Apple", Price = 20 },
+            new Product() { Id = 2, Name = "Pear", Price = 30 },
+            new Product() { Id = 3, Name = "Orange", Price = 40 }
+        };
+
+        public List<Product> GetAllProducts()=> Products;
+      
+
+        public Product GetById(int id) => Products.FirstOrDefault(item => item.Id == id);
+
+        public bool Add(Product product)
+        {
+            try
+            {
+                Products.Add(new Product()
+                {
+                    Id = Products.Count + 1,
+                    Name = product.Name,
+                    Price = product.Price
+
+                });
+                return true;
+            }
+
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return false;
+            }
+
+            return false;
+        }
+
+
+    }
+}
